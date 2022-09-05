4f7facdcfe749238fb3f2f9d6d8cca366686b8ce
Avraham Appel <aappel248@gmail.com>
2022-09-05 19:18:10 -0400

    Implement custom iterator for chunks

--- src/separators.rs
+++ src/separators.rs
@@ -1,12 +1,57 @@
+struct IntersperseChunks<I: Iterator> {
+    inner: I,
+    item: I::Item,
+    count: usize,
+    chunk_length: usize,
+}
+
+impl<I: Iterator> IntersperseChunks<I> {
+    fn new(inner: I, item: I::Item, chunk_length: usize) -> Self {
+        Self {
+            inner,
+            item,
+            chunk_length,
+            count: 0,
+        }
+    }
+}
+
+impl<I> Iterator for IntersperseChunks<I>
+where
+    <I as Iterator>::Item: Clone,
+    I: Iterator,
+{
+    type Item = I::Item;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.count == self.chunk_length {
+            self.count = 0;
+            Some(self.item.clone())
+        } else {
+            self.count += 1;
+            self.inner.next()
+        }
+    }
+}
+
+trait ToIntersperseChunks
+where
+    Self: Sized + Iterator,
+{
+    fn intersperse_chunks(self, item: Self::Item, chunk_length: usize) -> IntersperseChunks<Self> {
+        IntersperseChunks::new(self, item, chunk_length)
+    }
+}
+
+impl<I> ToIntersperseChunks for I where I: Iterator {}
+
 /// Insert separators in a number string.
 fn insert_separators(num_str: String) -> String {
     num_str
         .chars()
         .rev()
+        .intersperse_chunks(',', 3)
         .collect::<Vec<_>>()
-        .chunks(3)
-        .collect::<Vec<_>>()
-        .join([','].as_slice())
         .into_iter()
         .rev()
         .collect()
