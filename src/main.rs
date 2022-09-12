9a717922f95c2bba4ad83b45326224fc366dd8b6
Avraham Appel <aappel248@gmail.com>
2022-09-11 21:56:55 -0400

    Refactor output to component

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,12 @@ use web_sys::HtmlInputElement;
 use yew::prelude::*;
 
 mod input;
+mod output;
 mod separators;
 mod state;
 
 use input::Input;
-use separators::WithSeparators;
+use output::Output;
 use state::{Action, State};
 
 fn input_event_value(evt: Event) -> String {
@@ -17,63 +18,6 @@ fn input_event_value(evt: Event) -> String {
         .dyn_into::<HtmlInputElement>()
         .unwrap()
         .value()
-}
-
-macro_rules! age_html {
-    ($age:ident) => {{
-        let label = stringify!($age).to_string();
-
-        // Depluralize if necessary
-        let label = if $age == "1" {
-            &label[..label.len() - 1]
-        } else {
-            &label
-        };
-
-        html! {
-            <>
-                <b>{ $age }</b>
-                { format!(" {} old", label) }
-            </>
-        }
-    }};
-}
-
-/// Format the output of the age as Html
-fn output(state: &State) -> Html {
-    if state.name.is_empty() {
-        return html! {};
-    }
-
-    if let Some(birthday) = state.birthday {
-        let duration = state
-            .current_time
-            .signed_duration_since(birthday.and_hms(0, 0, 0));
-        let days = duration.num_days();
-        let years = (days / 365).to_string().with_separators();
-        let months = (days / 30).to_string().with_separators();
-        let hours = duration.num_hours().to_string().with_separators();
-        let minutes = duration.num_minutes().to_string().with_separators();
-        let seconds = duration.num_seconds().to_string().with_separators();
-        let days = days.to_string().with_separators();
-
-        html! {
-            <>
-                <h2>{ "Hello " } {&state.name} { "!" }</h2>
-
-                <p>{ "You are:" }</p>
-
-                <p>{ age_html!(years) }</p>
-                <p>{ age_html!(months) }</p>
-                <p>{ age_html!(days) }</p>
-                <p>{ age_html!(hours) }</p>
-                <p>{ age_html!(minutes) }</p>
-                <p>{ age_html!(seconds) }</p>
-            </>
-        }
-    } else {
-        html! { <p><i>{ "Enter a valid birthday" }</i></p> }
-    }
 }
 
 #[function_component(YourAge)]
@@ -97,12 +41,21 @@ fn your_age() -> Html {
         })
     };
 
-    let output = output(&state);
+    let duration = match state.birthday {
+        Some(birthday) => Some(
+            state
+                .current_time
+                .signed_duration_since(birthday.and_hms(0, 0, 0)),
+        ),
+        None => None,
+    };
+
+    let name = state.name.clone();
 
     html! {
         <>
             <Input {name_callback} {birthday_callback} />
-            {output}
+            <Output {name} {duration} />
         </>
     }
 }
---
+++ src/output.rs
@@ -0,0 +1,65 @@
+use chrono::Duration;
+use yew::prelude::*;
+
+use crate::separators::WithSeparators;
+
+#[derive(Eq, PartialEq, Properties)]
+pub struct Props {
+    pub name: String,
+    pub duration: Option<Duration>,
+}
+
+macro_rules! age_html {
+    ($age:ident) => {{
+        let label = stringify!($age).to_string();
+
+        // Depluralize if necessary
+        let label = if $age == "1" {
+            &label[..label.len() - 1]
+        } else {
+            &label
+        };
+
+        html! {
+            <>
+                <b>{ $age }</b>
+                { format!(" {} old", label) }
+            </>
+        }
+    }};
+}
+
+/// Format the output of the age as Html
+#[function_component(Output)]
+pub fn output(Props { duration, name }: &Props) -> Html {
+    if name.is_empty() {
+        return html! {};
+    }
+
+    if let Some(duration) = duration {
+        let days = duration.num_days();
+        let years = (days / 365).to_string().with_separators();
+        let months = (days / 30).to_string().with_separators();
+        let hours = duration.num_hours().to_string().with_separators();
+        let minutes = duration.num_minutes().to_string().with_separators();
+        let seconds = duration.num_seconds().to_string().with_separators();
+        let days = days.to_string().with_separators();
+
+        html! {
+            <>
+                <h2>{ "Hello " } {name} { "!" }</h2>
+
+                <p>{ "You are:" }</p>
+
+                <p>{ age_html!(years) }</p>
+                <p>{ age_html!(months) }</p>
+                <p>{ age_html!(days) }</p>
+                <p>{ age_html!(hours) }</p>
+                <p>{ age_html!(minutes) }</p>
+                <p>{ age_html!(seconds) }</p>
+            </>
+        }
+    } else {
+        html! { <p><i>{ "Enter a valid birthday" }</i></p> }
+    }
+}
