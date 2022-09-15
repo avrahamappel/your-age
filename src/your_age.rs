7d64eee744ccf56cb69e0df19c14aa7878ee949d
Avraham Appel <aappel248@gmail.com>
2022-09-14 21:22:05 -0400

    Refactor to regular use_effect

--- src/your_age.rs
+++ src/your_age.rs
@@ -1,5 +1,3 @@
-use std::rc::Rc;
-
 use chrono::{Local, NaiveDate, NaiveDateTime};
 use gloo_timers::callback::Interval;
 use wasm_bindgen::JsCast;
@@ -17,107 +15,43 @@ fn input_event_value(evt: Event) -> String {
         .value()
 }
 
-pub enum Action {
-    Tick,
-    UpdateName(String),
-    UpdateBirthday(String),
-}
-
-#[derive(Clone)]
-pub struct State {
-    pub name: String,
-    pub birthday: Option<NaiveDate>,
-    pub current_time: NaiveDateTime,
-}
-
-impl Reducible for State {
-    type Action = Action;
-
-    fn reduce(self: Rc<Self>, action: Self::Action) -> Rc<Self> {
-        use Action::*;
-
-        let state = match Rc::try_unwrap(self) {
-            Ok(state) => state,
-            Err(rc) => (*rc).clone(),
-        };
-
-        match action {
-            Tick => state.update_time(),
-            UpdateName(name) => state.update_name(name),
-            UpdateBirthday(birthday) => state.update_birthday(birthday),
-        }
-        .into()
-    }
-}
-
-fn current_time() -> NaiveDateTime {
+fn get_current_time() -> NaiveDateTime {
     Local::now().naive_local()
-}
-
-impl State {
-    pub fn new() -> Self {
-        Self {
-            name: String::new(),
-            birthday: None,
-            current_time: current_time(),
-        }
-    }
-
-    fn update_time(self) -> Self {
-        Self {
-            current_time: current_time(),
-            ..self
-        }
-    }
-
-    fn update_name(self, name: String) -> Self {
-        Self { name, ..self }
-    }
-
-    fn update_birthday(self, birthday: String) -> Self {
-        Self {
-            birthday: NaiveDate::parse_from_str(&birthday, "%F").ok(),
-            ..self
-        }
-    }
 }
 
 #[function_component(YourAge)]
 pub fn your_age() -> Html {
-    let state = use_reducer(State::new);
+    let name = use_state(String::new);
+    let birthday = use_state(|| None);
+    let current_time = use_state(get_current_time);
 
     // TODO Add a hook here that will push the birthday and name to the URL query
     // Later we can use that to implement a share button
 
     let _interval = use_state({
-        let state = state.clone();
-        move || Interval::new(1000, move || state.dispatch(Action::Tick))
+        let current_time = current_time.clone();
+        move || Interval::new(1000, move || current_time.set(get_current_time()))
     });
 
     let name_callback = {
-        let state = state.clone();
-        Callback::from(move |evt: Event| state.dispatch(Action::UpdateName(input_event_value(evt))))
+        let name = name.clone();
+        Callback::from(move |evt: Event| name.set(input_event_value(evt)))
     };
 
     let birthday_callback = {
-        let state = state.clone();
+        let birthday = birthday.clone();
         Callback::from(move |evt: Event| {
-            state.dispatch(Action::UpdateBirthday(input_event_value(evt)))
+            birthday.set(NaiveDate::parse_from_str(input_event_value(evt).as_str(), "%F").ok())
         })
     };
 
-    let duration = state.birthday.map(|birthday| {
-        state
-            .current_time
-            .signed_duration_since(birthday.and_hms(0, 0, 0))
-    });
-
-    let name = state.name.clone();
+    let duration =
+        birthday.map(|birthday| current_time.signed_duration_since(birthday.and_hms(0, 0, 0)));
 
     html! {
         <>
             <Input {name_callback} {birthday_callback} />
-            <Output {name} {duration} />
+            <Output name={(*name).clone()} {duration} />
         </>
     }
 }
