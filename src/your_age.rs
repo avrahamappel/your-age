50727683739945985be9d3b5cc041bfce95acb91
Avraham Appel <aappel248@gmail.com>
2022-09-18 08:51:51 -0400

    Push inputs to URL string
    REfactored back name and birthday to be part of a reducer. Still need
    to move the builder function into the impl as a `new` method.
    
    This is currently broken, as the page reloads every time an input
    changes. Also, it is not setting the initial state from the URL query.

--- src/your_age.rs
+++ src/your_age.rs
@@ -1,11 +1,81 @@
+use std::collections::HashMap;
+use std::fmt::Display;
+use std::rc::Rc;
+
 use chrono::{Local, NaiveDate, NaiveDateTime};
 use gloo_timers::callback::Interval;
-use wasm_bindgen::JsCast;
+use url::form_urlencoded::{self, Serializer};
+use wasm_bindgen::{JsCast, UnwrapThrowExt};
 use web_sys::HtmlInputElement;
 use yew::prelude::*;
 
 use crate::input::Input;
 use crate::output::Output;
+
+const DATE_FORMAT: &str = "%F";
+
+enum QueryParamsAction {
+    UpdateName(String),
+    UpdateBirthday(String),
+}
+
+#[derive(Clone)]
+struct QueryParams {
+    name: String,
+    birthday: Option<NaiveDate>,
+}
+
+impl Reducible for QueryParams {
+    type Action = QueryParamsAction;
+
+    fn reduce(self: Rc<Self>, action: Self::Action) -> Rc<Self> {
+        let params = match Rc::try_unwrap(self) {
+            Ok(params) => params,
+            Err(rc) => (*rc).clone(),
+        };
+
+        match action {
+            QueryParamsAction::UpdateName(name) => params.update_name(name),
+            QueryParamsAction::UpdateBirthday(birthday) => params.update_birthday(birthday),
+        }
+        .into()
+    }
+}
+
+impl Display for QueryParams {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let mut serializer = Serializer::new(String::new());
+
+        if !self.name.is_empty() {
+            serializer.append_pair("name", &self.name);
+        }
+
+        if let Some(birthday) = self.birthday {
+            serializer.append_pair("birthday", &birthday.format(DATE_FORMAT).to_string());
+        }
+
+        write!(f, "{}", serializer.finish())
+    }
+}
+
+impl QueryParams {
+    fn update_name(self, name: String) -> Self {
+        QueryParams { name, ..self }.set_location_search()
+    }
+
+    fn update_birthday(self, birthday: String) -> Self {
+        let birthday = NaiveDate::parse_from_str(&birthday, DATE_FORMAT).ok();
+        QueryParams { birthday, ..self }.set_location_search()
+    }
+
+    fn set_location_search(self) -> Self {
+        gloo_utils::window()
+            .location()
+            .set_search(self.to_string().as_str())
+            .expect_throw("Couldn't update URL query");
+        self
+    }
+}
 
 fn input_event_value(evt: Event) -> String {
     evt.target()
@@ -19,10 +89,27 @@ fn get_current_time() -> NaiveDateTime {
     Local::now().naive_local()
 }
 
+fn state_from_query() -> QueryParams {
+    let query = gloo_utils::window()
+        .location()
+        .search()
+        .expect_throw("Couldn't get query string");
+    let query_map = form_urlencoded::parse(query.as_bytes()).collect::<HashMap<_, _>>();
+
+    QueryParams {
+        name: query_map
+            .get("name")
+            .map(|name| name.to_string())
+            .unwrap_or_default(),
+        birthday: query_map
+            .get("birthday")
+            .and_then(|val| NaiveDate::parse_from_str(val, DATE_FORMAT).ok()),
+    }
+}
+
 #[function_component(YourAge)]
 pub fn your_age() -> Html {
-    let name = use_state(String::new);
-    let birthday = use_state(|| None);
+    let state = use_reducer(state_from_query);
     let current_time = use_state(get_current_time);
 
     // TODO Add a hook here that will push the birthday and name to the URL query
@@ -34,24 +121,29 @@ pub fn your_age() -> Html {
     });
 
     let name_callback = {
-        let name = name.clone();
-        Callback::from(move |evt: Event| name.set(input_event_value(evt)))
-    };
-
-    let birthday_callback = {
-        let birthday = birthday.clone();
+        let state = state.clone();
         Callback::from(move |evt: Event| {
-            birthday.set(NaiveDate::parse_from_str(input_event_value(evt).as_str(), "%F").ok())
+            state.dispatch(QueryParamsAction::UpdateName(input_event_value(evt)))
         })
     };
 
-    let duration =
-        birthday.map(|birthday| current_time.signed_duration_since(birthday.and_hms(0, 0, 0)));
+    let birthday_callback = {
+        let state = state.clone();
+        Callback::from(move |evt: Event| {
+            state.dispatch(QueryParamsAction::UpdateBirthday(input_event_value(evt)))
+        })
+    };
+
+    let duration = state
+        .birthday
+        .map(|birthday| current_time.signed_duration_since(birthday.and_hms(0, 0, 0)));
+
+    let name = state.name.clone();
 
     html! {
         <>
             <Input {name_callback} {birthday_callback} />
-            <Output name={(*name).clone()} {duration} />
+            <Output {name} {duration} />
         </>
     }
 }
