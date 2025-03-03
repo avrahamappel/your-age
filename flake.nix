7c0720fdcc5f710112343f3d28d82a1fd93137bd
Avraham Appel <aappel248@gmail.com>
2025-03-02 21:27:07 -0500

    Tauri plan

---
+++ flake.nix
@@ -0,0 +1,49 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    naersk.url = "github:nix-community/naersk";
+    naersk.inputs.nixpkgs.follows = "nixpkgs";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    fenix.url = "github:nix-community/fenix";
+    fenix.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs =
+    { flake-utils
+    , naersk
+    , nixpkgs
+    , fenix
+    , ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = (import nixpkgs) {
+          inherit system;
+          overlays = [ fenix.overlays.default ];
+        };
+
+        naersk' = pkgs.callPackage naersk { };
+      in
+      {
+        defaultPackage = naersk'.buildPackage {
+          src = ./.;
+        };
+
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            bacon
+            trunk
+            rust-analyzer
+            (pkgs.fenix.stable.withComponents [
+              "cargo"
+              "clippy"
+              "rust-src"
+              "rustc"
+              "rustfmt"
+            ])
+          ];
+        };
+      }
+    );
+}
---
+++ todo.md
@@ -0,0 +1,11 @@
+# Deploy with Tauri
+
+## Checklist
+
+- [x] Nix shell
+- [ ] See if this thing still compiles in 2025
+- [ ] (Optionally) Upgrade to 2024 edition
+- [ ] Add Tauri https://tauri.app/start/frontend/trunk/
+- [ ] Deploy
+- [ ] Add icons
+- [ ] Automate builds using GHA
